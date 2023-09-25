5ed4cdea30f69772164db2c2c368d892729c457e
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-09-25 16:17:50 -0300

    Arreglos

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,21 +1,19 @@
 import classes from './Form.module.css';
 
 const Form = () => {
-    return (
-        <div className={classes['form-background']}>
-            <div>
-                <h1>LA COSA</h1>
+  return (
+    <div className={classes['form-background']}>
+      <div>
+        <h1>LA COSA</h1>
                 
-                <form action="" className={classes['form-container']}>
-                    <h2>Unirse a una partida</h2>
-                    <label htmlFor="">Nombre de la partida</label>
-                    <br />
-                    <input type="text" required/>
-                    <br />
-                    <button>Unirse</button>
-                </form>
-            </div>
-        </div>
+        <form action="" className={classes['form-container']}>
+          <h2>Unirse a una partida</h2>
+          <label htmlFor="">Nombre de la partida</label>
+          <input type="text" required/>
+          <button>Unirse</button>
+        </form>
+      </div>
+    </div>
     )
 }
 
