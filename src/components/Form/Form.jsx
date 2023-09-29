c25ca6f8481e727af441b8d968a04600d73942c7
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-09-29 20:31:36 -0300

    Agregado

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,11 +5,12 @@ const Form = () => {
     <div className={classes['form-background']}>
       <div>
         <h1>LA COSA</h1>
-                
         <form action="" className={classes['form-container']}>
           <h2>Unirse a una partida</h2>
           <label htmlFor="">Nombre de la partida</label>
           <input type="text" required/>
+          <label htmlFor="playerID">Tu nombre</label>
+          <input type="text" name="" id="" required/>
           <button>Unirse</button>
         </form>
       </div>
