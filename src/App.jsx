d1e7ae440864977dc503a76fe37f96b73cd86801
Tomi Gonzalia <tomigonzalia@gmail.com>
2023-10-01 18:11:12 -0300

    fixing lint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
 import React from 'react'
-import Table from './components/Table/Table'
 
 
 function App() {
   return (
     <>
-      <Table nOfPlayers={5} />
+
     </>
   )
 }
