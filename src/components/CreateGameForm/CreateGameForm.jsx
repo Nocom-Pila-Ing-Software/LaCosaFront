af3eb7f0164f9e7a4fa727314e2adaa4e55fc493
facuayala00 <facundo.ayala@mi.unc.edu.ar>
2023-10-01 16:43:18 -0300

    title change

--- src/components/CreateGameForm/CreateGameForm.jsx
+++ src/components/CreateGameForm/CreateGameForm.jsx
@@ -6,7 +6,7 @@ const CreateGameForm = () => {
     <div className={classes['form-background']}>
         <h1>LA COSA</h1>
         <form action="" className={classes['form-container']}>
-          <h2>Crear una partida</h2>
+          <h2>Crear partida</h2>
           <input type="text" required id='name' placeholder='Nombre del jugador'/>
           <input type="text" required id='name' placeholder='Nombre de la partida'/>
           <button>Crear</button>
