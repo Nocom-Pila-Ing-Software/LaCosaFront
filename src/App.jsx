ca8a678ca9c546113b9bb4edb1f78456d0f2ec67
facuayala00 <facundo.ayala@mi.unc.edu.ar>
2023-10-01 17:03:17 -0300

    added detect react version eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import './App.css'
-import CreateGameForm from './components/CreateGameForm/CreateGameForm'
-import React from 'react'
+import './App.css';
+import CreateGameForm from './components/CreateGameForm/CreateGameForm';
+import React from 'react';
 
 function App() {
   return (
