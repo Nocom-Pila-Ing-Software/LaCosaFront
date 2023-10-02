393e54fd1a75413110e0c08df77fadeb8c1e7323
facuayala00 <facundo.ayala@mi.unc.edu.ar>
2023-10-02 00:41:44 -0300

    button and change to table

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-
-import './App.css'
-import JoinGameForm from './components/JoinGameForm/JoinGameForm'
+import './App.css';
+import JoinGameForm from './components/JoinGameForm/JoinGameForm';
 import CreateGameForm from './components/CreateGameForm/CreateGameForm';
-import React from 'react'
-
+import Table from './components/Table/Table'; // Asegúrate de importar el componente Table
+import React, { useState } from 'react';
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const handleStartGame = () => {
+    // Cambia el estado de la partida a "iniciada" cuando se hace clic en el botón "Iniciar partida"
+    setGameStarted(true);
+  };
 
   return (
-    <>
-      <CreateGameForm />
-      <JoinGameForm />
-
-    </>
-  )
+    <div className="App">
+      {gameStarted ? (
+        <Table nOfPlayers={4}/>
+      ) : (
+        <>
+          <CreateGameForm onStartGame={handleStartGame} />
+          <JoinGameForm />
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/CreateGameForm/CreateGameForm.jsx
+++ src/components/CreateGameForm/CreateGameForm.jsx
@@ -1,18 +1,52 @@
+import React, { useState } from 'react';
 import classes from '../styles/form-style.module.css';
-import React from 'react';
 
-const CreateGameForm = () => {
+const CreateGameForm = ({ onStartGame }) => {
+  const [playerName, setPlayerName] = useState('');
+  const [gameName, setGameName] = useState('');
+  const [gameCreated, setGameCreated] = useState(false);
+
+  const handleStartGame = () => {
+    // Llama a la función onStartGame para indicar que la partida ha comenzado
+    onStartGame();
+  };
+
+  const handleCreateGame = () => {
+    // Realiza la lógica para crear la partida aquí si es necesario
+    // Luego, establece gameCreated en true para mostrar el botón "Iniciar partida"
+    if (playerName.trim()!=='' && gameName.trim()!==''){
+      setGameCreated(true);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className={classes['form-background']}>
       <h1>LA COSA</h1>
-      <form action="" className={classes['form-container']}>
+      <form action="" className={classes['form-container']}onSubmit={handleSubmit}>
         <h2>Crear partida</h2>
-        <input type="text" required id='name' placeholder='Nombre del jugador' />
-        <input type="text" required id='name' placeholder='Nombre de la partida' />
-        <button>Crear</button>
+        <input
+          type="text"
+          required
+          placeholder="Nombre del jugador"
+          value={playerName}
+          onChange={(e) => setPlayerName(e.target.value)}
+        />
+        <input
+          type="text"
+          required
+          placeholder="Nombre de la partida"
+          value={gameName}
+          onChange={(e) => setGameName(e.target.value)}
+        />
+        <button onClick={handleCreateGame}>Crear partida</button>
+        {gameCreated && (<button onClick={handleStartGame}>Iniciar partida</button>)}
       </form>
     </div>
   );
-}
+};
 
 export default CreateGameForm;
