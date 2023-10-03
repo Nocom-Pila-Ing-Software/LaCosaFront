bdc49e66a5094678e623a5b666de1cce1ac61d80
Tomas Martinez <tomasmartinez@mi.unc.edu.ar>
2023-10-03 01:12:41 -0300

    Fix

--- test/JoinGameForm.test.js
+++ test/JoinGameForm.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import MockAdapter from 'axios-mock-adapter';
-import JoinGameForm from './JoinGameForm';
-import { URL_BACKEND } from '../../utils/constants';
+import '@testing-library/jest-dom';
+import  axios  from 'axios';
+import JoinGameForm from '../src/components/JoinGameForm/JoinGameForm';
+import { URL_BACKEND } from '../src/utils/constants';
 
-const axiosMock = new MockAdapter(axios);
+//const axiosMock = new MockAdapter(axios);
 
 describe ('JoinGameForm' , () =>{
-  it('renders correctly', () => {
+  test("renders correctly", () => {
     render(<JoinGameForm />);
     expect(screen.getByText('Unirse a una partida')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Nombre del jugador')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Nombre de la partida')).toBeInTheDocument();
     expect(screen.getByText('Unirse')).toBeInTheDocument();
     });
-  it('disables the "Unirse" button when modal is open', () =>{
+  test("disables the Unirse button when modal is open", () =>{
     render(<JoinGameForm />);
     const playerNameInput = screen.getByPlaceholderText('Nombre del jugador');
     const roomNameInput = screen.getByPlaceholderText('Nombre de la partida');
@@ -27,7 +27,7 @@ describe ('JoinGameForm' , () =>{
     fireEvent.click(joinGameButton);
     expect(joinGameButton).toBeDisabled();
   });
-  it('opens the modal when "Unirse" is clicked', () =>{
+  test("opens the modal when Unirse is clicked", () =>{
     render(<JoinGameForm />);
     const playerNameInput = screen.getByPlaceholderText('Nombre del jugador');
     const roomNameInput = screen.getByPlaceholderText('Nombre de la partida');
@@ -41,7 +41,7 @@ describe ('JoinGameForm' , () =>{
     expect(screen.getByText('Quieres salir?')).toBeInTheDocument();
     expect(screen.getByText('Salir')).toBeInTheDocument();
   });
-  it('closes the modal when "Salir" is clicked', () =>{
+  test("closes the modal when Salir is clicked", () =>{
     render(<JoinGameForm />);
     const playerNameInput = screen.getByPlaceholderText('Nombre del jugador');
     const roomNameInput = screen.getByPlaceholderText('Nombre de la partida');
@@ -56,7 +56,7 @@ describe ('JoinGameForm' , () =>{
     expect(screen.queryByText('Quieres salir?')).not.toBeInTheDocument();
     expect(screen.queryByText('Salir')).not.toBeInTheDocument();
   });
-  it('enables the "Unirse" button when modal is closed' , () =>{
+  test("enables the Unirse button when modal is closed" , () =>{
     render(<JoinGameForm />);
     const playerNameInput = screen.getByPlaceholderText('Nombre del jugador');
     const roomNameInput = screen.getByPlaceholderText('Nombre de la partida');
@@ -69,7 +69,7 @@ describe ('JoinGameForm' , () =>{
     fireEvent.click(screen.getByText('Salir'));
     expect(joinGameButton).not.toBeDisabled();
   });
-  it('shows an error message when the room name is not valid' , async () => {
+  test("shows an error message when the room name is not valid" , async () => {
     render(<JoinGameForm />);
     const playerNameInput = screen.getByPlaceholderText('Nombre del jugador');
     const roomNameInput = screen.getByPlaceholderText('Nombre de la partida');
