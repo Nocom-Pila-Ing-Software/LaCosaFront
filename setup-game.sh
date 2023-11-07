df3dd6b302b5ec16d8af26f08a15e73143143695
Lautaro Bachmann <laurigbach@gmail.com>
2023-11-06 22:00:14 -0300

    Add warnings when not running either front or back

--- setup-game.sh
+++ setup-game.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-# This will open a browser with 4 tabs, one for each player
-# the script will automatically log all players into the game
+# Function to display a warning message and exit
+display_warning_and_exit() {
+  echo "$1"
+  exit 1
+}
+
+curl "http://localhost:5173/" -s  > /dev/null || display_warning_and_exit "WARNING: hace falta que el front esté corriendo!"
+curl "http://127.0.0.1:8000" -s > /dev/null || display_warning_and_exit "WARNING: hace falta que el back esté corriendo!"
+
+# If both curl commands succeed, continue with the rest of the script
+# This will open a browser with N tabs, one for each player
+# You can define amount of players in a constant on tests/
+# The script will automatically log all players into the game
 npx playwright test tests/ --headed
