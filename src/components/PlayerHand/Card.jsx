624f4fdcbbfe09c755de71a28dcd18158189a11f
tomigonzalia <tomigonzalia@gmail.com>
2023-11-09 10:08:29 -0300

    still fixing

--- src/components/PlayerHand/Card.jsx
+++ src/components/PlayerHand/Card.jsx
@@ -2,30 +2,23 @@ import React, { useState } from 'react';
 import classes from './Card.module.css'
 import PropTypes from 'prop-types';
 
-
 const Card = (props) => {
   const playCard = () => {
     const { id, name, description } = props;
-    console.log(props.name);
-    console.log(props.cardsToTrade);
-    console.log(props.cardsToDefend);
     props.onCardClick(id, name, description);
-
   }
   const cardName = 'card-' + props.name.replace(/\s+/g, '-');
-  const isPlayable = props.cardsToPlay.some(card => card.name === props.name && (card.playable === true || card.discardable === true))
+  const isPlayable = props.cardsToPlay.some(card => card.name === props.name && card.playable === true)
   const isTradeable = props.cardsToTrade.some(card => card.name === props.name && card.usable)
   const canDefend = props.cardsToDefend.some(card => card.name === props.name && card.usable === true)
-
   return (
     <div className={`${classes[cardName]} 
     ${classes.background_img} 
     ${classes.card} 
     ${props.id === props.selectedCardID ? classes.selected : ''}
-    ${((isTradeable && isPlayable)) ? classes.canPlay : classes.cannotPlay}
+    ${((isTradeable || isPlayable)) ? classes.canPlay : classes.cannotPlay}
     `}
       onClick={playCard}>
-
     </div>
   )
 }
